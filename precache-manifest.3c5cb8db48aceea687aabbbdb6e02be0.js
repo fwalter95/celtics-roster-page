32d0f8eb4ce346c9aab8b6438b10ca4011b62002
walter_f <felix.walter@nayoki.de>
2020-07-21 14:09:51 +0200

    Updates

---
+++ precache-manifest.3c5cb8db48aceea687aabbbdb6e02be0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4eb52d7980b0ea29fe87e41fa5501508",
+    "url": "/celtics-roster-page/index.html"
+  },
+  {
+    "revision": "62d79d5489418e57dfd6",
+    "url": "/celtics-roster-page/static/css/main.9accb2c3.chunk.css"
+  },
+  {
+    "revision": "8961004a6b589eb17a4c",
+    "url": "/celtics-roster-page/static/js/2.e1a0f1bd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/celtics-roster-page/static/js/2.e1a0f1bd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "62d79d5489418e57dfd6",
+    "url": "/celtics-roster-page/static/js/main.c68afbe6.chunk.js"
+  },
+  {
+    "revision": "6ff0b6cacc9071683237",
+    "url": "/celtics-roster-page/static/js/runtime-main.dbe67e4a.js"
+  }
+]);
